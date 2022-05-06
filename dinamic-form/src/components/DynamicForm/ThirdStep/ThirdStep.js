800ed3c06bac9b44cf53f94a564a4db9d2b51b60
vanessa sanchez <vanessa.sanchezmc@gmail.com>
2022-05-06 18:20:53 -0500

    add step two and three

--- dinamic-form/src/components/DynamicForm/ThirdStep/ThirdStep.js
+++ dinamic-form/src/components/DynamicForm/ThirdStep/ThirdStep.js
@@ -1,9 +1,35 @@
-import './ThirdStep.css'
+import { Col, Container, Row, FloatingLabel, Form } from "react-bootstrap";
+import "./ThirdStep.css";
 
 const ThirdStep = () => {
   return (
-    <div>ThirdStep</div>
-  )
-}
+    <div>
+      <h2>Third step</h2>
+      <h4>Do you want to add any comment?</h4>
+      <Container className="p-5 text-center">
+        <Row className="justify-content-md-center">
+          <Col lg="10">
+            <FloatingLabel
+              controlId="floatingTextArea"
+              label="Comments"
+              className="mb-3"
+            >
+              <Form.Control
+                as="textarea"
+                placeholder="Comments"
+                onChange={(e) => console.log(e)}
+              />
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <div className="send-form">
+          <p className="result p-3" onClick={() => console.log("view result")}>
+            View Result 🙌
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-export default ThirdStep
+export default ThirdStep;
